574fef4bdb5b341581e01ec71ea50a269dd195a4
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-01-29 18:58:08 +0200

    created entity from Book class

--- src/main/java/viikko2/bookstore_t5/domain/Book.java
+++ src/main/java/viikko2/bookstore_t5/domain/Book.java
@@ -1,9 +1,15 @@
 package viikko2.bookstore_t5.domain;
 
-import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String author;
     private int publicationYear;
