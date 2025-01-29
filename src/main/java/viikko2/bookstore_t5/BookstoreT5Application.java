e3a391c602e938788d5ee3855df2e0320bca1a95
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-01-29 19:43:39 +0200

    commandlinerunner comman added

--- src/main/java/viikko2/bookstore_t5/BookstoreT5Application.java
+++ src/main/java/viikko2/bookstore_t5/BookstoreT5Application.java
@@ -1,13 +1,43 @@
 package viikko2.bookstore_t5;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import viikko2.bookstore_t5.domain.Book;
+import viikko2.bookstore_t5.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreT5Application {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreT5Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreT5Application.class, args);
+	}
+
+		@Bean
+	public CommandLineRunner studentDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of students");
+			repository.save(new Book("Aku Annka", "Don Johnson"));
+			repository.save(new Book("Havukka-ahon ajattelija", "Veikko Huovinen"));	
+			
+			repository.save(new Book("Musta Kostaja", "Adam Smithee"));
+			
+			log.info("fetch all students");
+			for (Book student : repository.findAll()) {
+				log.info(student.toString());
+			}
+			log.info("haetaan kaikki Naamiot");
+			for (Book student : repository.findByTitle("Naamio")) {
+				log.info(student.toString());
+			}
+
+		};
 	}
 
 }
